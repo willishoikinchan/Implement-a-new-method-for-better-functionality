ee153ae48ce7fde517d946da8294ed40a7a013fe
willishoikinchan <161003707+willishoikinchan@users.noreply.github.com>
2024-02-23 07:35:01 +0000

    Implemented a new method for better functionality

---
+++ Beautiful World.py
@@ -0,0 +1,14 @@
+# beautifulworld.py
+
+def main():
+    user_input = get_user_input()
+    print_user_input(user_input)
+
+def get_user_input():
+    return input("beautiful World ")
+
+def print_user_input(user_input):
+    print("beautiful World", user_input)
+
+if __name__ == "__main__":
+    main()
